34591f818c7400a1532fab7927db82ab94085fe6
Duy <duynguyen1442007@gmail.com>
2024-06-30 16:47:41 +0700

    update

--- Todo-App-Backend/database/schemaV1.sql
+++ Todo-App-Backend/database/schemaV1.sql
@@ -1,3 +1,21 @@
 -- Create a new database called 'DatabaseName'
 CREATE DATABASE postgres 
-CREATE SCHEMA public
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users(
+    id uuid PRIMARY KEY uuid_generate_v4 (),
+    name varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    role varchar(255) NOT NULL,
+    static_token varchar(1000)
+)
+CREATE SEQUENCE roles_id_seq
+CREATE TABLE roles(
+    id integer NOT NULL PRIMARY KEY DEAFFAULT nextval('roles_id_seq'),
+    table varchar(255) NOT NULL,
+    role varchar(255) NOT NULL,
+    create boolean NOT NULL,
+    read boolean NOT NULL,
+    update boolean NOT NULL,
+    delete boolean NOT NULL
+)
