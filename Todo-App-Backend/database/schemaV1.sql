967788e4a6ecf311bd44611e70a82022e8f20955
Duy Nguyen <duynguyen1442007@gmail.com>
2024-08-14 11:08:20 +0700

    update db and docker  compose

--- Todo-App-Backend/database/schemaV1.sql
+++ Todo-App-Backend/database/schemaV1.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE postgres 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users(
-    id uuid PRIMARY KEY uuid_generate_v4 (),
+    id uuid PRIMARY KEY uuid_generate_v4(),
     name varchar(255) NOT NULL,
     email varchar(255) UNIQUE NOT NULL,
     password varchar(255) NOT NULL,
@@ -11,11 +11,21 @@ CREATE TABLE users(
 )
 CREATE SEQUENCE roles_id_seq
 CREATE TABLE roles(
-    id integer NOT NULL PRIMARY KEY DEAFFAULT nextval('roles_id_seq'),
+    id integer NOT NULL PRIMARY KEY DEFAULT nextval('roles_id_seq'),
     table varchar(255) NOT NULL,
     role varchar(255) NOT NULL,
     create boolean NOT NULL,
     read boolean NOT NULL,
     update boolean NOT NULL,
     delete boolean NOT NULL
+)
+CREATE SEQUENCE sessions_id_seq
+CREATE TABLE "sessions"(
+    id integer NOT NULL PRIMARY KEY DEAFAULT nextval('sessions_id_seq'),
+    user_id uuid NOT NULL,
+    access_tokens varchar(1000) NOT NULL
+    refresh_tokens varchar(1000) NOT NULL
+    expires timestamp NOT NULL
+    ip varchar(255) NOT NULL
+    origin varchar(255) NOT NULL
 )
