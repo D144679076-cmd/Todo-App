18991cde3a1af3996dce4c407d78908a050e10c6
Duy Nguyen <duynguyen1442007@gmail.com>
2024-09-15 15:53:36 +0700

    add registerByEmail

--- Todo-App-Backend/api/auth/auth.routes.ts
+++ Todo-App-Backend/api/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import app from "../../index.js";
 import type { Request, Response } from "express";
-import { loginByEmail } from "./auth.controller.js";
+import { loginByEmail, registerByEmail } from "./auth.controller.js";
 app.route("/auth").post(async (req: Request, res: Response) => {
   await loginByEmail(req, res);
 });
-app.route("/user/register").post(async (req: Request, res: Response) => {});
+app.route("/user/register").post(async (req: Request, res: Response) => {
+  await registerByEmail(req, res);
+});
