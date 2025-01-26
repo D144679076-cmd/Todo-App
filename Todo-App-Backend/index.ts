aab559bfec28d570cb6acc1b70cad50d817329b4
Duynguyen <duynguyen1442007@gmail.com>
2025-01-26 21:33:56 +0700

    fix setup

--- Todo-App-Backend/index.ts
+++ Todo-App-Backend/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+import { env } from "node:process";
+configDotenv({ path: "./.env" });
 const app = express();
-app.listen(8055, () => {
-  console.log("Server started on port 8055");
+app.listen(env.PUBLIC_PORT ?? 8055, () => {
+  console.log(`Server started on port ${env.PUBLIC_PORT ?? 8055}`);
 });
 export default app;
