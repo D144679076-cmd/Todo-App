d08e907111da1c49fa2aa466d4585c1464579fc7
Duynguyen <duynguyen1442007@gmail.com>
2025-01-27 16:50:10 +0700

    update

--- Todo-App-Backend/api/user/user.routes.ts
+++ Todo-App-Backend/api/user/user.routes.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { loginByEmail, registerByEmail } from "./user.controller.js";
-const userRouter = express.Router();
-userRouter.route("/").get(async (req: Request, res: Response) => {
+const router = express.Router();
+router.get("/", async (req: Request, res: Response) => {
   res.status(200).json({
     message: "Hello World",
   });
 });
-userRouter.route("/auth").post(async (req: Request, res: Response) => {
+router.post("/auth", async (req: Request, res: Response) => {
   await loginByEmail(req, res);
 });
-userRouter.route("/register").post(async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   console.log("register");
   await registerByEmail(req, res);
 });
-export default userRouter;
+export default router;
