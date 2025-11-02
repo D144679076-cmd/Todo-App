082a1b64470e830d79e73b4bf919ab5535b84456
Duynguyen <duynguyen1442007@gmail.com>
2025-11-02 20:23:42 +0700

    fix vite config

--- Todo-App-Frontend/vite.config.js
+++ Todo-App-Frontend/vite.config.js
@@ -1,20 +1,27 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
+    plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
 
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
+    server: {
+        host: true,
+        allowedHosts: ["localhost", "pm.lynx-lab.org"]
+    },
+    preview: {
+        allowedHosts: ["localhost", "pm.lynx-lab.org"]
+    }
 });
